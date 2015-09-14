05df8fcfdd64cf0d3c8b463a33b46ca0087760c1
zhenyiluo <eric@Zhenyis-MacBook-Pro.local>
2015-09-14 02:01:10 -0400

    Number of Islands ||

--- src/NumberOfIslands||/Solution.java
+++ src/NumberOfIslands||/Solution.java
@@ -14,41 +14,73 @@ public class Solution {
      * @param operators an array of point
      * @return an integer array
      */
+    static int[] id;
+    static int[] sz;
+    static boolean[][] visited;
+    public static int find(int x){
+        while(x != id[x]){
+            id[x] = id[id[x]];
+            x = id[x];
+        }
+        return x;
+    }
+    
+    public static void union(int p, int q){
+        int i = find(p);
+        int j = find(q);
+        if(i == j){
+            return ;
+        }
+        if(sz[i] < sz[j]){
+            id[i] = j;
+            sz[j] += sz[i];
+        }else{
+            id[j] = i;
+            sz[i] += sz[j];
+        }
+    }
     public static final int[] dx = {-1, 1, 0, 0};
-    public static final int[] dy = {0, 0, -1, 1};
+    public static final int[] dy = {0, 0, 1, -1}; 
     public List<Integer> numIslands2(int n, int m, Point[] operators) {
         // Write your code here
         List<Integer> ret = new LinkedList<Integer>();
-        if(operators == null){
+        if(operators == null || operators.length == 0){
             return ret;
         }
-        boolean[][] visited = new boolean[n][m];
-        List<HashSet<Pair>> lists = new LinkedList<HashSet<Pair>>();
-        for(int i = 0; i < operators.length; i++){
-            ArrayList<Point> neighbours = new ArrayList<Point>();
-            int x = operators[i].x;
-            int y = operators[i].y;
-            for(int i = 0; i < 4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+        id = new int[n * m];
+        sz = new int[n * m];
+        for(int i = 0; i < n * m; i++){
+            id[i] = i;
+            sz[i] = 1;
+        }
+        
+        int cnt = 0;
+        int len = operators.length;
+        visited = new boolean[n][m];
+        for(int i = 0; i < len; i++){
+            Point point = operators[i];
+            int x = point.x;
+            int y = point.y;
+            if(visited[x][y]){
+                ret.add(cnt);
+                continue;
+            }
+            cnt++;
+            visited[x][y] = true;
+            int p = find(x * m + y);
+            for(int j = 0; j < 4; j++){
+                int nx = x + dx[j];
+                int ny = y + dy[j];
                 if(nx >= 0 && ny >= 0 && nx < n && ny < m && visited[nx][ny]){
-                    neighbours.add(new Point(nx, ny));
+                    int q = find(nx * m + ny);
+                    if(p != q){
+                        cnt --;
+                        union(p, q);
+                        p = find(x * m + y);
+                    }
                 }
             }
-            if(neighbours.size()== 0){
-                HashSet<Pair> hs = new HashSet<Pair>();
-                hs.add(new Pair(x, y));
-                lists.add(hs);
-                ret.add(lists.size());
-                visited[x][y] = true;
-            }else{
-                List<HashSet<Pair>> tmp = lists;
-                lists = new LinkedList<HashSet<Pair>>();
-                for(HashSet<Pair> hs : tmp){
-                
-                } 
-            }
-            
+            ret.add(cnt);
         }
         return ret;
     }
