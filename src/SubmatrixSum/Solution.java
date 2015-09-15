7ffa9dc232b52c21386f4d042d35f335e19e5299
Zhenyi Luo <zhenyiluo@gmail.com>
2015-09-15 18:57:29 -0400

    Submatrix sum

--- src/SubmatrixSum/Solution.java
+++ src/SubmatrixSum/Solution.java
@@ -7,21 +7,34 @@ public class Solution {
         // Write your code here
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][] sum = new int[m][n];
-        sum[0][0] = matrix[0][0];
-        for(int i = 1; i < m; i++){
-            sum[i][0] = sum[i-1][0] + matrix[i][0];
-        }
+        int[][] sum = new int[m+1][n+1];
         
-        for(int i = 1; i < n; i++){
-            sum[0][i] = sum[0][i-1] + matrix[0][i];
-        }
-        
-        for(int i = 1; i < m; i++){
-            for(int j = 1; j < n; j++){
-                sum[i][j] = matrix[i][j] + sum[i-1][j] + sum[i][j-1] 
-                -sum[i-1][j-1];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                sum[i+1][j+1] = matrix[i][j] + sum[i][j+1] + sum[i+1][j] 
+                -sum[i][j];
             }
         }
+        
+        int[][] ret = new int[2][2];
+        
+        for(int l = 0; l <= m; l++){
+            for(int h = l+1; h <= m; h++){
+                HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+                for(int k = 0; k <= n; k++){
+                    int tmp = sum[h][k] - sum[l][k];
+                    if(hm.containsKey(tmp)){
+                        int prevK = hm.get(tmp);
+                        ret[0][0] = l;
+                        ret[0][1] = prevK;
+                        ret[1][0] = h-1;
+                        ret[1][1] = k-1;
+                    }else{
+                        hm.put(tmp, k);
+                    }
+                }
+            }
+        }
+        return ret;
     }
 }
