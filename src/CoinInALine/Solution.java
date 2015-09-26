9bc35d052ce4daf13529140022de6e3287978e25
Zhenyi Luo <zhenyiluo@gmail.com>
2015-09-26 16:18:19 -0400

    Coin in a line

---
+++ src/CoinInALine/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    /**
+     * @param n: an integer
+     * @return: a boolean which equals to true if the first player will win
+     */
+    public boolean firstWillWin(int n) {
+       return n % 3 != 0;
+    }
+}
