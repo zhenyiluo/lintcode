4ca256830ac13bf94181ee0c5bc16247da36f6be
Zhenyi Luo <zhenyiluo@gmail.com>
2015-10-11 16:41:12 -0400

    Flip bits

---
+++ src/FlipBits/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     *@param a, b: Two integer
+     *return: An integer
+     */
+    public static int bitSwapRequired(int a, int b) {
+        // write your code here
+        return Integer.bitCount(a ^ b);
+    }
+};
+
