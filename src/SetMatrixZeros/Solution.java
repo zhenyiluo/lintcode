78165c34d9c482f0b444ab8f34ff230e50b70aa0
Zhenyi Luo <zhenyiluo@gmail.com>
2015-10-12 15:51:10 -0400

    Set matrix zeros

---
+++ src/SetMatrixZeros/Solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    /**
+     * @param matrix: A list of lists of integers
+     * @return: Void
+     */
+    public void setZeroes(int[][] matrix) {
+        // write your code here
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
