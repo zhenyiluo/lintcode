e6aec92938dde8b1007f87fe595f190091f0d28f
zhenyiluo <zhenyiluo@gmail.com>
2015-10-18 22:19:07 -0400

    Add BFS Solution

--- src/TopologicalSorting/Solution.java
+++ src/TopologicalSorting/Solution.java
@@ -1,3 +1,62 @@
+// BFS:
+/**
+ * Definition for Directed graph.
+ * class DirectedGraphNode {
+ *     int label;
+ *     ArrayList<DirectedGraphNode> neighbors;
+ *     DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    /**
+     * @param graph: A list of Directed graph node
+     * @return: Any topological order for the given graph.
+     */    
+    public ArrayList<DirectedGraphNode> topSort(ArrayList<DirectedGraphNode> graph) {
+        // write your code here
+        ArrayList<DirectedGraphNode> ret = new ArrayList<>();
+        if(graph == null || graph.size() == 0){
+            return ret;
+        }
+        HashMap<DirectedGraphNode, Integer> hm = new HashMap<>();
+        for(DirectedGraphNode node : graph){
+            for(DirectedGraphNode neighbor : node.neighbors){
+                if(hm.containsKey(neighbor)){
+                    hm.put(neighbor, hm.get(neighbor) + 1);
+                }else{
+                    hm.put(neighbor, 1);
+                }
+            }
+        }
+        
+        Queue<DirectedGraphNode> q = new LinkedList<>();
+        for(DirectedGraphNode node : graph){
+            if(!hm.containsKey(node)){
+                q.add(node);
+                ret.add(node);
+            }
+        }
+        
+        while(!q.isEmpty()){
+            DirectedGraphNode node = q.poll();
+            for(DirectedGraphNode neighbor : node.neighbors){
+                if(hm.containsKey(neighbor)){
+                    int val = hm.get(neighbor) -1;
+                    if(val == 0){
+                        hm.remove(neighbor);
+                        ret.add(neighbor);
+                        q.add(neighbor);
+                    }else{
+                        hm.put(neighbor, val);
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+}
+
+
 /**
  * Definition for Directed graph.
  * class DirectedGraphNode {
